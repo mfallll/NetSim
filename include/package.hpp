0a108a063aeb368cb72c3b1129f8b14dcffd63e7
Martyna Falowska <mfalowska@student.agh.edu.pl>
2024-12-27 16:39:38 +0100

    znalazłam coś w instrukcji

--- include/package.hpp
+++ include/package.hpp
@@ -1,5 +1,16 @@
 #include "types.hpp"
+#include <set>
 
 class Package {
-
+public:
+    Package();
+    Package(ElementID id) : id_(id) {};
+    Package(Package &&package) : id_(package.id_) {};
+    Package &operator=(Package &&package);
+    ElementID get_id() { return id_; }
+    ~Package();
+private:
+    ElementID id_;
+    static std::set<ElementID> assigned_IDs_;
+    static std::set<ElementID> freed_IDs_;
 };
