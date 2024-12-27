ec2280d1b847e5aa70b90feae78de8f6168b26b7
lizzygac <egaczynska@student.agh.edu.pl>
2024-12-27 19:11:50 +0100

    yay

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -3,16 +3,15 @@
 Package PackageQueue::pop() {
         Package package;
         PackageQueueType t = queuetype_;
- 
         switch(t) {
             case PackageQueueType::FIFO:
-                package = std::move(queue_.back());
-                queue_.pop_back();
-                
-            case PackageQueueType::LIFO:
                 package = std::move(queue_.front());
                 queue_.pop_front();
-        break;
+                break;
+            case PackageQueueType::LIFO:
+                package = std::move(queue_.back());
+                queue_.pop_back();
+                break;
         }
         return package;
 }
