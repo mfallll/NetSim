29572b4609f46e2240078852d57129359f1c5c94
Martyna Falowska <mfalowska@student.agh.edu.pl>
2025-01-15 11:38:12 +0100

    exercise defined

--- include/factory.hpp
+++ include/factory.hpp
@@ -1,11 +1,28 @@
 #ifndef FACTORY_HPP_
 #define FACTORY_HPP_
 
+#include <stack>
 #include "config.hpp"
 #include "types.hpp"
 #include "package.hpp"
 #include "helpers.hpp"
 #include "storage_types.hpp"
+
+template <typename Node> class NodeCollection {
+public:
+    void add(Node &&node);
+    void remove_by_id(ElementID id);
+
+    using stack = typename std::stack<Node>;
+    using iterator = typename stack::iterator;
+    using const_iterator = typename stack::const_iterator;
+
+
+
+    NodeCollection<Node>::iterator find_by_id(ElementID id);
+    NodeCollection<Node>::const_iterator find_by_id(ElementID id);
+};
+
 
 class factory{
     factory();
