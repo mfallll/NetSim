2fb288f4078f5dfd30c8efff4ebf93526f05c961
AdamGawlas328 <agawlas@student.agh.edu.pl>
2025-01-15 11:01:31 +0100

    Mergus

--- include/factory.hpp
+++ include/factory.hpp
@@ -12,7 +12,7 @@ class factory{
     bool is_consistent(); // Sprawdzenie spójnosci
     void do_deliveries(); // Odwolanie do rampy
     void do_package_passing(); // odwolanie do przekazywania polproduktow
-    void do_work(); //
+    void do_work(); // odwolanie do robotnikow
 };
 
 
--- src/factory.cpp
+++ src/factory.cpp
@@ -1,3 +1,22 @@
 #include "factory.hpp"
 
 
+factory::factory() {
+
+}
+
+bool factory::is_consistent() {
+    return false;
+}
+
+void factory::do_deliveries() {
+
+}
+
+void factory::do_package_passing() {
+
+}
+
+void factory::do_work() {
+
+}
