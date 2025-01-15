078ec9ec45a587c1ce171f2b6311fd0cb4661ef2
lizzygac <egaczynska@student.agh.edu.pl>
2025-01-15 12:48:03 +0100

    please please please split

--- src/Factory.cpp
+++ src/Factory.cpp
@@ -1,5 +1,8 @@
 #include "Factory.hpp"
-
+#include <exception>
+#include <vector>
+#include <string>
+#include <sstream>
 
 Factory::Factory() {
     //potrrzebuje zmiany
@@ -36,6 +39,51 @@ void Factory::do_work() {
 
 }
 
+std::vector<std::string> split(const std::string& str, char delimiter)
+{
+    std::stringstream test(str);
+    std::string segment;
+    std::vector<std::string> seglist;
+
+    while(std::getline(test, segment, delimiter))
+    {
+        seglist.push_back(segment);
+    }
+    return seglist;
+}
+
+ParsedLineData parse_line(std::string& line){
+    std::vector<std::string> tokens;
+    std::string token;
+
+    std::istringstream token_stream(line);
+    char delimiter = ' ';
+
+    while (std::getline(token_stream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    std::map<std::string, ElementType> map_element_types{
+            {"LOADING_RAMP", ElementType::RAMP},
+            {"WORKER", ElementType::WORKER},
+            {"STOREHOUSE", ElementType::STOREHOUSE},
+            {"LINK", ElementType::LINK}
+    };
+
+    ParsedLineData parsed_line_data;
+
+    try {
+        parsed_line_data.element_type = map_element_types.at(tokens[0]);
+        std::for_each(tokens.begin(), tokens.end(), [&](const std::string & please){
+            auto value = split(please, '=');
+            parsed_line_data.parameters[value[0]] = value[1];
+        });
+    }
+    catch( std::out_of_range& e) { throw std::exception(); }
+
+    return parsed_line_data;
+}
+
 Factory load_factory_structure(std::istream& is){
     Factory factory;
     std::string line;
@@ -45,4 +93,5 @@ Factory load_factory_structure(std::istream& is){
         }
     }
 
+    //TO DO
 }
