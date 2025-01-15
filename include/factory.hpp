4d50b5303cfbaf303d550fa403223e638ae5c00c
AdamGawlas328 <agawlas@student.agh.edu.pl>
2025-01-15 10:51:14 +0100

    factory

---
+++ include/factory.hpp
@@ -0,0 +1,7 @@
+class factory{
+    factory();
+    bool is_consistent(); // Sprawdzenie spójnosci
+    void do_deliveries(); // Odwolanie do rampy
+    void do_package_passing(); // odwolanie do przekazywania polproduktow
+    void do_work(); //
+};
