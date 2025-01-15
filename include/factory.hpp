8b274dff16ed8fb5678db038a38b3ceaa2fc71f5
lizzygac <egaczynska@student.agh.edu.pl>
2025-01-15 10:45:52 +0100

    naglowek

---
+++ include/factory.hpp
@@ -0,0 +1,10 @@
+#ifndef FACTORY_HPP_
+#define FACTORY_HPP_
+
+#include "config.hpp"
+#include "types.hpp"
+#include "package.hpp"
+#include "helpers.hpp"
+#include "storage_types.hpp"
+
+#endif // FACTORY_HPP_
