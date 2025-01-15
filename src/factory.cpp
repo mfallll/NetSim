c5f05839210ed0bb37726c95760e6cb12513d0ff
AdamGawlas328 <agawlas@student.agh.edu.pl>
2025-01-15 11:15:50 +0100

    Kolejny dzien, kolejny push

--- src/factory.cpp
+++ src/factory.cpp
@@ -6,6 +6,19 @@ factory::factory() {
 }
 
 bool factory::is_consistent() {
+    /* Użyj algorytmu DFS do przejścia grafu, dla każdej rampy. */
+
+//    kolor := mapowanie {PackageSender* -> kolor_węzła_enum}
+//    dla wszystkich ramp i dla wszystkich robotników jako `węzeł`:
+//    kolor[węzeł] = NIEODWIEDZONY
+//
+//    jeśli poniższa instrukcja rzuci wyjątek, zwróć FAŁSZ:  /* tj. sieć nie jest spójna */
+//    dla każdej rampy `rampa` w kolekcji wszystkich ramp w sieci:
+//    czy_nadawca_posiada_osiągalny_magazyn(rampa, kolor)
+//    w przeciwnym razie:
+//    zwróć PRAWDA  /* tj. sieć jest spójna */
+
+
     return false;
 }
 
